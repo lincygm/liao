21f7f39d9a6fcbd7225fbe76f5e48998a8f2102b
Arrow <852100921@qq.com>
2018-05-15 15:16:04 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/activity/CallVoiceActivity.java
+++ app/src/main/java/com/king/liaoba/mvp/activity/CallVoiceActivity.java
@@ -3,6 +3,7 @@ package com.king.liaoba.mvp.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,6 +33,7 @@ public class CallVoiceActivity extends Activity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_call);
     }
 }
