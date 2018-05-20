7435685b83cde6e2e744f7cec63005810cae2da4
gaomou <852100921@qq.com>
2018-05-21 07:26:59 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
+++ app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
@@ -1,15 +1,29 @@
 package com.king.liaoba.mvp.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.decoration.SpaceDecoration;
+import com.king.liaoba.App;
 import com.king.liaoba.Constants;
+import com.king.liaoba.bean.Root;
+import com.king.liaoba.http.APIRetrofit;
+import com.king.liaoba.http.APIService;
+import com.king.liaoba.mvp.adapter.ImageAdapter;
+import com.king.liaoba.util.DensityUtil;
 import com.liaoba.R;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import retrofit2.Retrofit;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * @author Jenly <a href="mailto:jenly1314@gmail.com">Jenly</a>
@@ -24,8 +38,8 @@ public class FollowFragment extends SimpleFragment {
     TextView tvTitle;
     @BindView(R.id.ivRight)
     ImageView ivRight;
-    @BindView(R.id.tvLogin)
-    TextView tvLogin;
+
+    EasyRecyclerView recyclerView = null;
 
     public static FollowFragment newInstance() {
 
@@ -46,12 +60,47 @@ public class FollowFragment extends SimpleFragment {
         tvTitle.setText(R.string.tab_follw);
     }
 
+    private void adapter(){
+        recyclerView = (EasyRecyclerView) getActivity().findViewById(R.id.photowall_recyclerView);
+        //recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        //recyclerView.setAdapter(adapter = new ImageAdapter(this, list));
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        //gridLayoutManager.setSpanSizeLookup(adapter.obtainGridSpanSizeLookUp(2));
+        recyclerView.setLayoutManager(gridLayoutManager);
+        SpaceDecoration itemDecoration = new SpaceDecoration(DensityUtil.dp2px(getContext(), 6));
+        //itemDecoration.setPaddingEdgeSide(true);
+        //itemDecoration.setPaddingStart(true);
+        //itemDecoration.setPaddingHeaderFooter(true);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
     @Override
     public void initData() {
 
+        Retrofit retrofit = APIRetrofit.getInstance();
+        APIService service =retrofit.create(APIService.class);
+        service.getFocusList(App.getSharedPreference("chatid"))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Root>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onNext(Root root) {
+
+                    }
+                });
+
     }
 
-    @OnClick({R.id.ivLeft, R.id.ivRight, R.id.tvLogin})
+    @OnClick({R.id.ivLeft, R.id.ivRight})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.ivLeft:
@@ -60,9 +109,7 @@ public class FollowFragment extends SimpleFragment {
             case R.id.ivRight:
                 startLogin();
                 break;
-            case R.id.tvLogin:
-                startLogin();
-                break;
+
         }
     }
 }
