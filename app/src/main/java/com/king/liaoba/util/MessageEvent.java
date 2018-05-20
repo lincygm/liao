01edad2a74355e65f00556d54576270caf5b40aa
gaomou <852100921@qq.com>
2018-05-21 07:27:42 +0800

    p

--- app/src/main/java/com/king/liaoba/util/MessageEvent.java
+++ app/src/main/java/com/king/liaoba/util/MessageEvent.java
@@ -1,10 +1,26 @@
 package com.king.liaoba.util;
 
-public class MessageEvent {
+public class MessageEvent<T> {
     private String message;
+    private T data;
+    public MessageEvent(String message,T data) {
+        this.message = message;
+        this.data = data;
+    }
 
-    public MessageEvent(String message) {
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
