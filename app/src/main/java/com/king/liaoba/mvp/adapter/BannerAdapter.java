4baadcd997b944ec18bf791b1b3315a48539adf5
Arrow <852100921@qq.com>
2018-05-23 16:31:15 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/adapter/BannerAdapter.java
+++ app/src/main/java/com/king/liaoba/mvp/adapter/BannerAdapter.java
@@ -3,35 +3,73 @@ package com.king.liaoba.mvp.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.jude.rollviewpager.adapter.StaticPagerAdapter;
+import com.king.liaoba.App;
+import com.king.liaoba.Constants;
+import com.king.liaoba.bean.PictureRoot;
+import com.king.liaoba.http.APIRetrofit;
+import com.king.liaoba.http.APIService;
 import com.liaoba.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Retrofit;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class BannerAdapter extends StaticPagerAdapter {
+    ArrayList<Ad> arr = new ArrayList<>();
 
     private Context ctx;
     private List<Ad> list;
        public BannerAdapter(Context ctx){
            this.ctx = ctx;
-            list = getAdList();
+            //list = getAdList();
+           getPicture();
         }
 
-    public static List<Ad> getAdList(){
-        ArrayList<Ad> arr = new ArrayList<>();
+    public  List<Ad> getAdList(){
         arr.add(new Ad("http://i2.hdslb.com/u_user/e97a1632329cac1fa6ab3362e7233a08.jpg","http://www.bilibili.com/topic/v2/1004.html"));
         arr.add(new Ad("http://i1.hdslb.com/u_user/80fcc32d0b5d3565377847bd9dd06dc3.jpg","http://www.bilibili.com/topic/1003.html"));
         arr.add(new Ad("http://i2.hdslb.com/u_user/f19f0e44328a4190a48acf503c737c50.png","http://yoo.bilibili.com/html/activity/cq2015/index.html"));
         arr.add(new Ad("http://i1.hdslb.com/u_user/7ee1aeadc8257f43fa6b806717c9c398.png","http://www.bilibili.com/html/activity-acsociety.html"));
         return arr;
     }
+    private void getPicture(){
+        Retrofit retrofit = APIRetrofit.getInstance();
+        APIService service =retrofit.create(APIService.class);
+        service.getImageList(Constants.getSharedPreference("chatid",ctx))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<PictureRoot>() {
+                    @Override
+                    public void onCompleted() {
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext( PictureRoot jsonBean) {
+                        if (jsonBean != null) {
+                            for(int i=0;i<jsonBean.getData().getGetdata().size();i++){
+                                arr.add(new Ad(jsonBean.getData().getGetdata().get(i).getPicurl(),""));
+                            }
+                        }
+                    }
+                });
+
+    }
         @Override
         public View getView(ViewGroup container, final int position) {
             ImageView imageView = new ImageView(ctx);
@@ -40,7 +78,6 @@ public class BannerAdapter extends StaticPagerAdapter {
             //加载图片
             Glide.with(ctx)
                     .load(list.get(position).getImage())
-                    .placeholder(R.drawable.default_image)
                     .into(imageView);
             //点击事件
             imageView.setOnClickListener(new View.OnClickListener() {
