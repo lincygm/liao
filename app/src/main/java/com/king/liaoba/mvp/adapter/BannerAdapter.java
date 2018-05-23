6d456692aa61129bc3efe1fe467a4842ee2a91d5
gaomou <852100921@qq.com>
2018-05-23 23:34:49 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/adapter/BannerAdapter.java
+++ app/src/main/java/com/king/liaoba/mvp/adapter/BannerAdapter.java
@@ -12,6 +12,7 @@ import com.bumptech.glide.Glide;
 import com.jude.rollviewpager.adapter.StaticPagerAdapter;
 import com.king.liaoba.App;
 import com.king.liaoba.Constants;
+import com.king.liaoba.bean.PictureList;
 import com.king.liaoba.bean.PictureRoot;
 import com.king.liaoba.http.APIRetrofit;
 import com.king.liaoba.http.APIService;
@@ -26,50 +27,23 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class BannerAdapter extends StaticPagerAdapter {
-    ArrayList<Ad> arr = new ArrayList<>();
-
-    private Context ctx;
+    List<PictureList> piclist = new ArrayList<>();
     private List<Ad> list;
-       public BannerAdapter(Context ctx){
+    private Context ctx;
+       public BannerAdapter(Context ctx,List<PictureList> pictureLists){
            this.ctx = ctx;
-            //list = getAdList();
-           getPicture();
+           this.piclist =pictureLists;
+           getAdList();
         }
 
     public  List<Ad> getAdList(){
-        arr.add(new Ad("http://i2.hdslb.com/u_user/e97a1632329cac1fa6ab3362e7233a08.jpg","http://www.bilibili.com/topic/v2/1004.html"));
-        arr.add(new Ad("http://i1.hdslb.com/u_user/80fcc32d0b5d3565377847bd9dd06dc3.jpg","http://www.bilibili.com/topic/1003.html"));
-        arr.add(new Ad("http://i2.hdslb.com/u_user/f19f0e44328a4190a48acf503c737c50.png","http://yoo.bilibili.com/html/activity/cq2015/index.html"));
-        arr.add(new Ad("http://i1.hdslb.com/u_user/7ee1aeadc8257f43fa6b806717c9c398.png","http://www.bilibili.com/html/activity-acsociety.html"));
-        return arr;
+        list = new ArrayList<>();
+           for(int i=0;i<piclist.size();i++){
+               list.add(new Ad(Constants.BASE_URL+piclist.get(i).getPicurl(),""));
+           }
+          return list;
     }
-    private void getPicture(){
-        Retrofit retrofit = APIRetrofit.getInstance();
-        APIService service =retrofit.create(APIService.class);
-        service.getImageList(Constants.getSharedPreference("chatid",ctx))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<PictureRoot>() {
-                    @Override
-                    public void onCompleted() {
-                    }
 
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext( PictureRoot jsonBean) {
-                        if (jsonBean != null) {
-                            for(int i=0;i<jsonBean.getData().getGetdata().size();i++){
-                                arr.add(new Ad(jsonBean.getData().getGetdata().get(i).getPicurl(),""));
-                            }
-                        }
-                    }
-                });
-
-    }
         @Override
         public View getView(ViewGroup container, final int position) {
             ImageView imageView = new ImageView(ctx);
@@ -79,18 +53,14 @@ public class BannerAdapter extends StaticPagerAdapter {
             Glide.with(ctx)
                     .load(list.get(position).getImage())
                     .into(imageView);
-            //点击事件
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ctx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(list.get(position).getUrl())));
-                }
-            });
             return imageView;
         }
 
         @Override
         public int getCount() {
+        if(list!=null)
             return list.size();
+        else
+            return 0;
         }
     }
