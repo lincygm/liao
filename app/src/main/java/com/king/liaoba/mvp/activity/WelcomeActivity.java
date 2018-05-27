4f0ad40b351e3ab121393780ca22fffcf2749de2
gaomou <852100921@qq.com>
2018-05-27 23:42:21 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/activity/WelcomeActivity.java
+++ app/src/main/java/com/king/liaoba/mvp/activity/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.king.liaoba.mvp.activity;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.animation.Animation;
-
+import android.Manifest;
 import com.king.base.SplashActivity;
 import com.king.liaoba.MainActivity;
 import com.liaoba.R;
@@ -15,6 +19,28 @@ public class WelcomeActivity extends SplashActivity {
     @Override
     public int getContentViewId() {
         return R.layout.activity_welcome;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public void requestPower() {
+        //判断是否已经赋予权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {//这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+            } else {
+                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
     }
 
     @Override
