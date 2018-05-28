22ffa8495b6275a945955b3fa9fc83fa73fdaf23
Arrow <852100921@qq.com>
2018-05-28 17:12:36 +0800

    g

--- app/src/main/java/com/king/liaoba/mvp/adapter/EasyVoiceAdapter.java
+++ app/src/main/java/com/king/liaoba/mvp/adapter/EasyVoiceAdapter.java
@@ -20,7 +20,10 @@ import com.king.liaoba.Constants;
 import com.king.liaoba.bean.VoiceListInfo;
 import com.king.liaoba.mvp.activity.SelfShowActivity;
 import com.king.liaoba.mvp.activity.VoiceChatViewActivity;
+import com.king.liaoba.util.MessageEvent;
 import com.liaoba.R;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.io.IOException;
 import java.util.List;
@@ -100,25 +103,10 @@ public class EasyVoiceAdapter extends RecyclerArrayAdapter <VoiceListInfo>{
             voice.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        //1 初始化mediaplayer
-                        final MediaPlayer mediaPlayer = new MediaPlayer();
-                        //2 设置到播放的资源位置 path 可以是网络 路径 也可以是本地路径
 
-                        try {
-                            mediaPlayer.setDataSource(Constants.BASE_URL+Constants.getSharedPreference("voicelibrary",mContext));
-                            //3 准备播放
-                            mediaPlayer.prepareAsync();
-                            //3.1 设置一个准备完成的监听
-                            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                                @Override
-                                public void onPrepared(MediaPlayer mp) {
-                                    // 4 开始播放
-                                    mediaPlayer.start();
-                                }
-                            });
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    Log.d("voice",Constants.BASE_URL+Constants.getSharedPreference("voicelibrary",mContext));
+
+                    EventBus.getDefault().post(new MessageEvent<>("play",""));
 
                 }
             });
