03d4bf38a1b50da59c1c4ceac4fd68815ba34972
Arrow <852100921@qq.com>
2018-05-29 19:08:51 +0800

    g

--- app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
+++ app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
@@ -2,20 +2,30 @@ package com.king.liaoba.mvp.fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
 import com.jude.easyrecyclerview.decoration.DividerDecoration;
+
+
 import com.jude.rollviewpager.Util;
 import com.king.liaoba.Constants;
 import com.king.liaoba.bean.Root;
 import com.king.liaoba.http.APIRetrofit;
 import com.king.liaoba.http.APIService;
-import com.king.liaoba.mvp.adapter.PersonWithAdAdapter;
+
+import com.king.liaoba.mvp.adapter.PersonViewHolder;
+
 import com.liaoba.R;
 
 
@@ -31,7 +41,7 @@ import rx.schedulers.Schedulers;
  * @since 2017/3/9
  */
 
-public class FollowFragment extends SimpleFragment {
+public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdapter.OnLoadMoreListener, SwipeRefreshLayout.OnRefreshListener{
 
 
     @BindView(R.id.foll_fans)
@@ -44,10 +54,13 @@ public class FollowFragment extends SimpleFragment {
     TextView tvTitle;
     @BindView(R.id.ivRight)
     ImageView ivRight;
-    PersonWithAdAdapter adapter;
+    RecyclerArrayAdapter adapter;
     EasyRecyclerView recyclerView_fans = null;
     EasyRecyclerView recyclerView_focus = null;
+    private Handler handler = new Handler();
 
+    private int page = 0;
+    private boolean hasNetWork = true;
 
     public static FollowFragment newInstance() {
 
@@ -68,14 +81,41 @@ public class FollowFragment extends SimpleFragment {
     }
 
     private void adapter(){
-        recyclerView_focus = (EasyRecyclerView) getActivity().findViewById(R.id.recyclerView);
-        recyclerView_focus.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView_focus.setProgressView(R.layout.view_progress);
-        DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY, Util.dip2px(getActivity(),0.5f), Util.dip2px(getActivity(),72),0);
-        recyclerView_focus.addItemDecoration(itemDecoration);
-        adapter = new PersonWithAdAdapter(getActivity());
-        //adapter.addAll(DataProvider.getPersonWithAds(0));
-        recyclerView_focus.setAdapterWithProgress(adapter);
+        recyclerView_fans = (EasyRecyclerView) getActivity().findViewById(R.id.photowall_recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView_fans.setLayoutManager(layoutManager);
+        DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY,Util.dip2px(getActivity(),16f), Util.dip2px(getActivity(),72),0);
+        itemDecoration.setDrawLastItem(false);
+        recyclerView_fans.addItemDecoration(itemDecoration);
+
+        recyclerView_fans.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Root>(getActivity()) {
+            @Override
+            public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                return new PersonViewHolder(parent);
+            }
+        });
+        adapter.setMore(R.layout.view_more, this);
+        adapter.setNoMore(R.layout.view_nomore);
+        adapter.setOnItemLongClickListener(new RecyclerArrayAdapter.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(int position) {
+                adapter.remove(position);
+                return true;
+            }
+        });
+        adapter.setError(R.layout.view_error, new RecyclerArrayAdapter.OnErrorListener() {
+            @Override
+            public void onErrorShow() {
+                adapter.resumeMore();
+            }
+
+            @Override
+            public void onErrorClick() {
+                adapter.resumeMore();
+            }
+        });
+        recyclerView_fans.setRefreshListener(this);
+        onRefresh();
     }
 
     @Override
@@ -102,6 +142,41 @@ public class FollowFragment extends SimpleFragment {
                     }
                 });
 
+    }
+    //第四页会返回空,意为数据加载结束
+    @Override
+    public void onLoadMore() {
+        Log.i("EasyRecyclerView","onLoadMore");
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //刷新
+                if (!hasNetWork) {
+                    adapter.pauseMore();
+                    return;
+                }
+              //  adapter.addAll(DataProvider.getPersonList(page));
+                page++;
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onRefresh() {
+        page = 0;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adapter.clear();
+                //刷新
+                if (!hasNetWork) {
+                    adapter.pauseMore();
+                    return;
+                }
+                //adapter.addAll(DataProvider.getPersonList(page));
+                page=1;
+            }
+        }, 2000);
     }
 
     @OnClick({R.id.ivLeft, R.id.ivRight,R.id.foll_focus,R.id.foll_fans})
