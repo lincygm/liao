62a558b270d4d8db830301ef751a8fc1bb959e87
gaomou <852100921@qq.com>
2018-05-30 23:47:27 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
+++ app/src/main/java/com/king/liaoba/mvp/fragment/FollowFragment.java
@@ -20,6 +20,8 @@ import com.jude.easyrecyclerview.decoration.DividerDecoration;
 
 import com.jude.rollviewpager.Util;
 import com.king.liaoba.Constants;
+import com.king.liaoba.bean.FriendsRoot;
+import com.king.liaoba.bean.JsonBean;
 import com.king.liaoba.bean.Root;
 import com.king.liaoba.http.APIRetrofit;
 import com.king.liaoba.http.APIService;
@@ -29,8 +31,12 @@ import com.king.liaoba.mvp.adapter.PersonViewHolder;
 import com.liaoba.R;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.OnClick;
+import cn.jpush.android.service.DataProvider;
 import retrofit2.Retrofit;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -58,7 +64,9 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
     EasyRecyclerView recyclerView_fans = null;
     EasyRecyclerView recyclerView_focus = null;
     private Handler handler = new Handler();
-
+    private List<String> focusList = new ArrayList<>();
+    private List<String> fansList = new ArrayList<>();
+    private List<JsonBean> userList = new ArrayList<>();
     private int page = 0;
     private boolean hasNetWork = true;
 
@@ -80,15 +88,15 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
         tvTitle.setText(R.string.tab_follw);
     }
 
-    private void adapter(){
-        recyclerView_fans = (EasyRecyclerView) getActivity().findViewById(R.id.photowall_recyclerView);
+    private void fansadapter(){
+        recyclerView_fans = (EasyRecyclerView) getActivity().findViewById(R.id.foll_fans_recycleview);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView_fans.setLayoutManager(layoutManager);
         DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY,Util.dip2px(getActivity(),16f), Util.dip2px(getActivity(),72),0);
         itemDecoration.setDrawLastItem(false);
-        recyclerView_fans.addItemDecoration(itemDecoration);
+       // recyclerView_fans.addItemDecoration(itemDecoration);
 
-        recyclerView_fans.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Root>(getActivity()) {
+        recyclerView_fans.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<JsonBean>(getActivity()) {
             @Override
             public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
                 return new PersonViewHolder(parent);
@@ -118,18 +126,83 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
         onRefresh();
     }
 
+    private void focusadapter(){
+        recyclerView_focus = (EasyRecyclerView) getActivity().findViewById(R.id.foll_fans_recycleview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView_focus.setLayoutManager(layoutManager);
+        DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY,Util.dip2px(getActivity(),16f), Util.dip2px(getActivity(),72),0);
+        itemDecoration.setDrawLastItem(false);
+        recyclerView_focus.addItemDecoration(itemDecoration);
+
+        recyclerView_focus.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<JsonBean>(getActivity()) {
+            @Override
+            public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                return new PersonViewHolder(parent);
+            }
+        });
+        adapter.setMore(R.layout.view_more, this);
+        adapter.setNoMore(R.layout.view_nomore);
+        adapter.setOnItemLongClickListener(new RecyclerArrayAdapter.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(int position) {
+                adapter.remove(position);
+                return true;
+            }
+        });
+        adapter.setError(R.layout.view_error, new RecyclerArrayAdapter.OnErrorListener() {
+            @Override
+            public void onErrorShow() {
+                adapter.resumeMore();
+            }
+
+            @Override
+            public void onErrorClick() {
+                adapter.resumeMore();
+            }
+        });
+        recyclerView_focus.setRefreshListener(this);
+        onRefresh();
+    }
+
+
     @Override
     public void initData() {
-
         Retrofit retrofit = APIRetrofit.getInstance();
         APIService service =retrofit.create(APIService.class);
         service.getFocusList(Constants.getSharedPreference("chatid",getActivity()))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Root>() {
+                .subscribe(new Observer<FriendsRoot>() {
                     @Override
                     public void onCompleted() {
+                        Log.d("DDS","getFocusList completes");
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(FriendsRoot root) {
+                            if(root!=null&&focusList!=null){
+                                focusList.clear();
+                                for(int i=0;i<root.getData().getGetdata().size();i++){
+                                    focusList.add(root.getData().getGetdata().get(i).getFocus_id());
+                                    getUserinfoByChatid(root.getData().getGetdata().get(i).getFocus_id());
+
+                                }
+                        }
+                    }
+                });
+
+        service.getFansList(Constants.getSharedPreference("chatid",getActivity()))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<FriendsRoot>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("DDS","getFansList completes");
                     }
 
                     @Override
@@ -137,12 +210,47 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
                     }
 
                     @Override
-                    public void onNext(Root root) {
-
+                    public void onNext(FriendsRoot root) {
+                        if(root!=null&&fansList!=null){
+                            fansList.clear();
+                            for(int i=0;i<root.getData().getGetdata().size();i++){
+                                    fansList.add(root.getData().getGetdata().get(i).getChat_id());
+                                //getUserinfoByChatid(root.getData().getGetdata().get(i).getChat_id());
+                            }
+                        }
                     }
                 });
 
+
     }
+
+    private void getUserinfoByChatid(String chatid){
+        Retrofit retrofit = APIRetrofit.getInstance();
+        APIService service =retrofit.create(APIService.class);
+        service.getUserInfoByChatid(chatid)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Root>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("DDS","getUserInfoByChatid completes");//最后走这个
+                        fansadapter();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Root root) {
+                        if(root!=null){
+                            userList.add(root.getData().getGetdata().get(0));
+                        }
+                    }
+                });
+    }
+
     //第四页会返回空,意为数据加载结束
     @Override
     public void onLoadMore() {
@@ -155,7 +263,9 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
                     adapter.pauseMore();
                     return;
                 }
-              //  adapter.addAll(DataProvider.getPersonList(page));
+              // adapter.addAll(DataProvider.getPersonList(page));
+               // adapter.addAll(userList);
+
                 page++;
             }
         }, 2000);
@@ -174,6 +284,7 @@ public class FollowFragment extends SimpleFragment implements  RecyclerArrayAdap
                     return;
                 }
                 //adapter.addAll(DataProvider.getPersonList(page));
+                adapter.addAll(userList);
                 page=1;
             }
         }, 2000);
