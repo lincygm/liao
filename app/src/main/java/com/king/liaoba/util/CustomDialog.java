075e586528376036c03078f5fefc2f3807e9d1dd
gaomou <852100921@qq.com>
2018-06-03 23:34:03 +0800

    p

---
+++ app/src/main/java/com/king/liaoba/util/CustomDialog.java
@@ -0,0 +1,56 @@
+package com.king.liaoba.util;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.content.Context;
+import com.liaoba.R;
+import com.wang.avi.AVLoadingIndicatorView;
+
+public class CustomDialog extends ProgressDialog {
+
+    AVLoadingIndicatorView avLoadingIndicatorView;
+
+    public CustomDialog(Context context)
+    {
+        super(context);
+    }
+
+    public CustomDialog(Context context, int theme)
+    {
+        super(context, theme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        init(getContext());
+    }
+
+    private void init(Context context)
+    {
+        //设置不可取消，点击其他区域不能取消，实际中可以抽出去封装供外包设置
+        setCancelable(false);
+        setCanceledOnTouchOutside(false);
+
+        setContentView(R.layout.progress_layout);
+        avLoadingIndicatorView = (AVLoadingIndicatorView)this.findViewById(R.id.progress);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        getWindow().setAttributes(params);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        avLoadingIndicatorView.show();
+    }
+
+    public void hide(){
+        super.hide();
+        avLoadingIndicatorView.hide();
+    }
+}
