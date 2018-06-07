215b312fbddf9dc0405dd60fc38250fbd00803fe
Arrow <852100921@qq.com>
2018-06-07 16:46:05 +0800

    f

---
+++ app/src/main/java/com/king/liaoba/bean/Charge.java
@@ -0,0 +1,22 @@
+package com.king.liaoba.bean;
+
+public class Charge {
+    private String money;
+    private String coin;
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public String getCoin() {
+        return coin;
+    }
+
+    public void setCoin(String coin) {
+        this.coin = coin;
+    }
+}
