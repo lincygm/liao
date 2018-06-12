e7fd4da0d0c83764e0e4789884ae2cb4c0c237b1
Arrow <852100921@qq.com>
2018-06-12 12:44:56 +0800

    p

---
+++ app/src/main/java/com/king/liaoba/mvp/presenter/FollowPresenter.java
@@ -0,0 +1,12 @@
+package com.king.liaoba.mvp.presenter;
+
+import com.king.liaoba.App;
+import com.king.liaoba.mvp.base.BasePresenter;
+import com.king.liaoba.mvp.view.IFollowView;
+
+public class FollowPresenter extends BasePresenter<IFollowView> {
+    public FollowPresenter(App app) {
+        super(app);
+    }
+
+}
