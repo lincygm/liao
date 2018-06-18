ae06a724cd94c4b52e7d1f92d08c3744fc7c76e4
lincy <852100921@qq.com>
2018-06-18 12:02:37 +0800

    p

--- app/src/main/java/com/king/liaoba/mvp/activity/MatchActivity.java
+++ app/src/main/java/com/king/liaoba/mvp/activity/MatchActivity.java
@@ -1,14 +1,13 @@
 package com.king.liaoba.mvp.activity;
 
-import android.animation.AnimatorSet;
-import android.animation.ArgbEvaluator;
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.king.liaoba.Constants;
 import com.king.liaoba.bean.Root;
@@ -16,6 +15,7 @@ import com.king.liaoba.http.APIRetrofit;
 import com.king.liaoba.http.APIService;
 import com.liaoba.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Retrofit;
 import rx.Observer;
@@ -24,12 +24,29 @@ import rx.schedulers.Schedulers;
 
 
 public class MatchActivity extends AppCompatActivity {
+
+    @BindView(R.id.title_close)
+    ImageView iv_close;
+    @BindView(R.id.title_name)
+    TextView tv_title;
+    @BindView(R.id.title_right)
+    TextView tv_right;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
         ButterKnife.bind(this);
-        getChatid();
+        tv_title.setText("");
+        tv_right.setVisibility(View.GONE);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                getChatid();
+            }
+        }.start();
     }
 
     private void getChatid(){
@@ -42,7 +59,6 @@ public class MatchActivity extends AppCompatActivity {
                 .subscribe(new Observer<Root>() {
                     @Override
                     public void onCompleted() {
-                        Log.d("DDS","getFocusList completes");
                     }
 
                     @Override
@@ -52,9 +68,7 @@ public class MatchActivity extends AppCompatActivity {
                     @Override
                     public void onNext(Root root) {
                         if(root!=null&&root.getData().getGetdata().size()>0){
-                            for(int i=0;i<root.getData().getGetdata().size();i++){
-
-                            }
+                           Log.d("MATCH",">>>>");
                         }
                     }
                 });
